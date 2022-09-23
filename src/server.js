62fe26a6e538a4fa085845d64ad1c69d0394e073
leejae honey <ljh23270@naver.com>
2022-09-23 13:22:41 +0900

    use env

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import postingRouter from "./routers/postingRouter";
 import { localsMiddleware } from "./middlewares";
@@ -14,9 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "honey",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+    // cookie: {
+    //   maxAge: 20000,
+    // },
   })
 );
 
